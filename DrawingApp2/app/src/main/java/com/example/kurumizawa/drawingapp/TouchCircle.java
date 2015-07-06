e30bdf6d234d8265f4e3779559e6b09fb592f098
楜澤圭史 <kurumizawa@fotome.jp>
2015-07-06 14:20:02 +0900

    Path描画の処理

--- DrawingApp2/app/src/main/java/com/example/kurumizawa/drawingapp/TouchCircle.java
+++ DrawingApp2/app/src/main/java/com/example/kurumizawa/drawingapp/TouchCircle.java
@@ -4,15 +4,15 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.view.MotionEvent;
 import android.view.View;
 
-/**
- * Created by kurumizawa on 2015/07/03.
- */
 public class TouchCircle extends View {
 
-    public int x = 100, y = 100;
+    private float posx = 0.0f;  //タッチした場所のx座標
+    private float posy = 0.0f;  //タッチした芭蕉のy座標
+    private Path path = null;   //パス
 
     public TouchCircle(Context context)
     {
@@ -21,38 +21,52 @@ public class TouchCircle extends View {
 
     protected void onDraw(Canvas canvas)
     {
+        super.onDraw(canvas);
+
         Paint paint = new Paint();
         //アンチエイリアスの設定
         paint.setAntiAlias(true);
         //円の色を設定(黒色)
         paint.setColor(Color.BLACK);
+        //描画は線のみ
+        paint.setStyle(Paint.Style.STROKE);
         //線の太さ
-        paint.setStrokeWidth(10);
+        paint.setStrokeWidth(6);
+        //線の端を丸くする
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        //線の繋ぎ目を丸くする
+        paint.setStrokeJoin(Paint.Join.ROUND);
         //円の描画
-        canvas.drawCircle(x, y, 50, paint);
+        if(path != null)
+        {
+            canvas.drawPath(path, paint);
+        }
     }
 
     public boolean onTouchEvent(MotionEvent e)
     {
+        //イベントごとに処理を設定する
         switch (e.getAction())
         {
             //タッチイベント(押された時)
             case MotionEvent.ACTION_DOWN:
-                x = (int)e.getX();
-                y = (int)e.getY();
-                invalidate();
+                path = new Path();
+                posx = e.getX();
+                posy = e.getY();
+                path.moveTo(e.getX(), e.getY());
                 break;
 
             //タッチイベント(移動中)
             case MotionEvent.ACTION_MOVE:
-                x = (int)e.getX();
-                y = (int)e.getY();
+                posx += (e.getX() - posx) / 1.4f;
+                posy += (e.getY() - posy) / 1.4f;
+                path.lineTo(posx, posy);
                 invalidate();
                 break;
 
             //タッチイベント(離れた時)
             case MotionEvent.ACTION_UP:
-                performClick();
+                path.lineTo(e.getX(), e.getY());
                 break;
 
             default:
